68ad96bdacdaa6566153db95d9fa4e12c67cf6c1
Zubayer Ahmed <zubayer.developer@gmail.com>
2018-05-11 22:03:58 +0600

    Test app without login manager

--- app/src/main/java/zubayer/note/MainActivity.java
+++ app/src/main/java/zubayer/note/MainActivity.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -17,6 +23,8 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.internal.ImageRequest;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -24,6 +32,10 @@ import com.facebook.login.widget.ProfilePictureView;
 import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -46,10 +58,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        genetatekeyHash();
 
+        genetatekeyHash();
+        FacebookSdk.sdkInitialize(getApplicationContext());
         loginButton = (LoginButton) findViewById(R.id.login_button);
         text = (TextView) findViewById(R.id.text);
         email = (TextView) findViewById(R.id.email);
@@ -58,23 +70,6 @@ public class MainActivity extends AppCompatActivity {
         profilePictureView = (ProfilePictureView) findViewById(R.id.profile);
         callbackManager = CallbackManager.Factory.create();
 
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                    }
-
-                    @Override
-                    public void onCancel() {
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                        text.setText("Login error");
-                    }
-                });
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
         loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -83,14 +78,18 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
                         try {
-                            text.setText(object.get("id").toString());
-                            email.setText(object.get("first_name").toString() + " " + object.get("last_name").toString());
-                            birthday.setText(object.get("email").toString());
-                            profilePictureView.setProfileId(loginResult.getAccessToken().getUserId());
+                            text.setText(object.getString("id"));
+                            email.setText(object.getString("first_name") + " " + object.getString("last_name"));
+                            birthday.setText(object.getString("email"));
 
-                            String url = response.getJSONObject().getJSONObject("picture").optJSONObject("data").getString("url");
-                            Picasso.get().load(url).into(imageView);
+                            URL img_value = new URL("https://graph.facebook.com/"+object.getLong("id")+"/picture?type=large");
+                            String photoUrl = img_value.toString();
+                            Glide.with(getApplicationContext()).load(photoUrl).thumbnail(0.5f)
+                                    .into(imageView);
+
                         } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (MalformedURLException e) {
                             e.printStackTrace();
                         }
                         Log.d("myresponse", loginResult.getAccessToken().getUserId());
@@ -98,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 });
 
                 Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,first_name,last_name,email,picture.type(large)");
+                parameters.putString("fields", "id,first_name,last_name,email,picture");
                 request.setParameters(parameters);
                 request.executeAsync();
             }
