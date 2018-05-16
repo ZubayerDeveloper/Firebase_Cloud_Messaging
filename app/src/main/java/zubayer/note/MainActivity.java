fcf4a35cba12f7468ef3578a5162bbfe18988cac
Zubayer Ahmed <zubayer.developer@gmail.com>
2018-05-16 15:25:57 +0600

    Test app

--- app/src/main/java/zubayer/note/MainActivity.java
+++ app/src/main/java/zubayer/note/MainActivity.java
@@ -4,18 +4,13 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -28,14 +23,8 @@ import com.facebook.internal.ImageRequest;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.facebook.login.widget.ProfilePictureView;
-import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -47,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
     TextView text, email, birthday;
     ImageView imageView;
     GraphRequest request;
-    ProfilePictureView profilePictureView;
 
     @Override
     public void onActivityReenter(int resultCode, Intent data) {
@@ -58,18 +46,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
         genetatekeyHash();
-        FacebookSdk.sdkInitialize(getApplicationContext());
+
         loginButton = (LoginButton) findViewById(R.id.login_button);
         text = (TextView) findViewById(R.id.text);
         email = (TextView) findViewById(R.id.email);
         birthday = (TextView) findViewById(R.id.birthday);
         imageView = (ImageView) findViewById(R.id.image);
-        profilePictureView = (ProfilePictureView) findViewById(R.id.profile);
         callbackManager = CallbackManager.Factory.create();
 
+
+
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
         loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -78,22 +69,19 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
                         try {
-                            text.setText(object.getString("id"));
+                            text.setText("Facebook id:"+object.getString("id"));
                             email.setText(object.getString("first_name") + " " + object.getString("last_name"));
                             birthday.setText(object.getString("email"));
 
-                            URL img_value = new URL("https://graph.facebook.com/"+object.getLong("id")+"/picture?type=large");
-                            String photoUrl = img_value.toString();
-                            Glide.with(getApplicationContext()).load(photoUrl).thumbnail(0.5f)
+                            Glide.with(getApplicationContext()).load(ImageRequest.getProfilePictureUri(object.getString("id"),500,500).toString()).thumbnail(0.5f)
                                     .into(imageView);
+                            Log.d("myresponse", loginResult.getAccessToken().getUserId());
+                            Log.d("myresponse", ImageRequest.getProfilePictureUri(object.getString("id"),500,500).toString());
 
                         } catch (JSONException e) {
                             e.printStackTrace();
-                        } catch (MalformedURLException e) {
-                            e.printStackTrace();
                         }
-                        Log.d("myresponse", loginResult.getAccessToken().getUserId());
-                    }
+                                            }
                 });
 
                 Bundle parameters = new Bundle();
